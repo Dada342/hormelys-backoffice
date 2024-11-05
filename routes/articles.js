0789916244ce1a57ef6169e42995885598026025
David <aldavid342@gmail.com>
2024-11-05 18:08:24 +0100

    Replace multer-storage-cloudinary with direct Cloudinary integration

--- routes/articles.js
+++ routes/articles.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const multer = require('multer');
-const { CloudinaryStorage } = require('multer-storage-cloudinary');
 const cloudinary = require('cloudinary').v2;
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
 const router = express.Router();
 const Article = require('../models/Article');
 const authMiddleware = require('../middlewares/authMiddleware');
