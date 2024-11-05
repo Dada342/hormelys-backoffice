8cf10f869f2332ea2f3855c7f960d0aac32537a9
David <aldavid342@gmail.com>
2024-11-05 18:15:31 +0100

    Remove any lingering references to multer-storage-cloudinary

--- routes/articles.js
+++ routes/articles.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const multer = require('multer');
 const cloudinary = require('cloudinary').v2;
-const { CloudinaryStorage } = require('multer-storage-cloudinary');
 const router = express.Router();
 const Article = require('../models/Article');
 const authMiddleware = require('../middlewares/authMiddleware');
